1dce169673d57beb3dc1d1ff55861d3c3d8ced9f
Snowy <snowycold1068@gmail.com>
2024-11-02 19:21:11 +0100

    bumped version

--- BetterCoinflips/Plugin.cs
+++ BetterCoinflips/Plugin.cs
@@ -9,8 +9,8 @@ namespace BetterCoinflips
     public class Plugin : Plugin<Config, Configs.Translations>
     {
         public override Version RequiredExiledVersion => new(8, 7, 0, 0);
-        public override Version Version => new(4, 5, 0);
-        public override string Author => "Miki_hero";
+        public override Version Version => new(6, 0, 0);
+        public override string Author => "Miki_hero - fork(snowycoldd)";
         public override string Name => "BetterCoinflips";
 
         public static Plugin Instance;
@@ -36,6 +36,8 @@ namespace BetterCoinflips
             Player.FlippingCoin += _eventHandler.OnCoinFlip;
             Map.SpawningItem += _eventHandler.OnSpawningItem;
             Map.FillingLocker += _eventHandler.OnFillingLocker;
+            Player.Died += _eventHandler.OnPlayerDied;
+            Player.Spawning += _eventHandler.OnPlayerSpawned;
         }
 
         private void UnRegisterEvents()
@@ -43,6 +45,8 @@ namespace BetterCoinflips
             Player.FlippingCoin -= _eventHandler.OnCoinFlip;
             Map.SpawningItem -= _eventHandler.OnSpawningItem;
             Map.FillingLocker -= _eventHandler.OnFillingLocker;
+            Player.Died -= _eventHandler.OnPlayerDied;
+            Player.Spawning -= _eventHandler.OnPlayerSpawned;
             _eventHandler = null;
         }
     }
