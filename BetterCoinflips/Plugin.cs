eac07a7eb3e510cfc4db8085346e7e969c9ca49f
Snowy <snowycold1068@gmail.com>
2024-11-05 21:22:57 +0100

    bumped version

--- BetterCoinflips/Plugin.cs
+++ BetterCoinflips/Plugin.cs
@@ -9,7 +9,7 @@ namespace BetterCoinflips
     public class Plugin : Plugin<Config, Configs.Translations>
     {
         public override Version RequiredExiledVersion => new(8, 7, 0, 0);
-        public override Version Version => new(6, 0, 0);
+        public override Version Version => new(6, 1, 0);
         public override string Author => "Miki_hero - fork(snowycoldd)";
         public override string Name => "BetterCoinflips";
 
